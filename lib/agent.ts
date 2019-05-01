20d0ea6c62777006bf5580cc2f457f8e3f98f11d
Christopher Blanchard <cablanchard@gmail.com>
2019-05-01 17:00:05 +0200

    Add documentation on how to use abstract agent

--- lib/agent.ts
+++ lib/agent.ts
@@ -8,7 +8,24 @@
 /**
  * Agent
  *
- * Object that exports a HTTP `http` method
+ * The sole requirement of this class is to implement a JSON request method
+ * that supports HTTP attributes defined in `HttpRequest`
+ *
+ * The underlying implementation (i.e. whether it uses node's `http` module,
+ * or browser XHR) is the responsibility of downstream client implementations.
+ * Namely @ideal-postcodes/core-browser and @ideal-postcodes/core-node
+ *
+ * @example
+ *
+ * ```javascript
+ * class AxiosAgent implements Agent {
+ *   public http(options, callback) {
+ *     axios.request(options)
+ *      .then(response => callback(undefined, response))
+ *      .catch(error => callback(error));
+ *   }
+ * }
+ * ```
  */
 export interface Agent {
   http: Http;
@@ -17,7 +34,7 @@ export interface Agent {
 /**
  * Request
  *
- * Dispatches HTTP request
+ * Dispatches HTTP JSON http request
  */
 export interface Http {
   (httpRequest: HttpRequest, callback: HttpCallback): void;
@@ -32,16 +49,22 @@ type HttpVerb = "GET" | "POST" | "DELETE" | "PUT" | "PATCH" | "DELETE";
  */
 export interface HttpRequest {
   method: HttpVerb;
+  // JSON request body
   body?: any;
+  // Time in milliseconds before request times out
   timeout: number;
+  // Base URL to hit (includes protocol, hostname and prefixing path)
   url: string;
+  // String map which represents HTTP headers to send
   header: Header;
+  // String map which represents query strings to attach to URL
   query: Query;
 }
 
 interface StringMap {
   [key: string]: string;
 }
+
 type Header = StringMap;
 type Query = StringMap;
 
@@ -55,8 +78,12 @@ export interface HttpCallback {
  * Represents HTTP Response of request
  */
 export interface HttpResponse {
+  // HTTP status code
   httpStatus: number;
+  // HTTP response headers
   headers: Header;
+  // JSON response body
   body: any;
+  // Original HTTP request
   httpRequest: HttpRequest;
 }
