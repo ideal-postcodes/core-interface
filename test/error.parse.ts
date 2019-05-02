e817f419fa4f1e45c0fbedda9d9d4b4ef9571f38
Christopher Blanchard <cablanchard@gmail.com>
2019-05-02 14:28:52 +0200

    Implement test for unrecognised API errors (fallback)

--- test/error.parse.ts
+++ test/error.parse.ts
@@ -1,4 +1,8 @@
-import { parse, IdpcResourceNotFoundError } from "../lib/error";
+import {
+  parse,
+  IdealPostcodesError,
+  IdpcResourceNotFoundError,
+} from "../lib/error";
 import { defaultResponse } from "./helper/index";
 import { assert } from "chai";
 
@@ -15,6 +19,57 @@ describe("parse", () => {
     };
     const error = parse(response);
     assert.instanceOf(error, IdpcResourceNotFoundError);
+  });
+
+  it("returns a generic error if body is not json object", () => {
+    const body = "foo";
+    const httpStatus = 888;
+    const response = {
+      ...defaultResponse,
+      ...{ httpStatus, body },
+    };
+    const error = parse(response);
+    assert.instanceOf(error, IdealPostcodesError);
+
+    assert.equal((error as IdealPostcodesError).httpStatus, httpStatus);
+    assert.equal((error as IdealPostcodesError).message, body);
+  });
+  it("returns a generic error if malformed message", () => {
+    const body = { code: 4040 };
+    const httpStatus = 888;
+    const response = {
+      ...defaultResponse,
+      ...{ httpStatus, body },
+    };
+    const error = parse(response);
+    assert.instanceOf(error, IdealPostcodesError);
+
+    assert.equal((error as IdealPostcodesError).httpStatus, httpStatus);
+  });
+  it("returns a generic error if malformed code", () => {
+    const body = {
+      message: "404 Page not found",
+    };
+    const httpStatus = 404;
+    const response = {
+      ...defaultResponse,
+      ...{ httpStatus, body },
+    };
+    const error = parse(response);
+    assert.instanceOf(error, IdealPostcodesError);
+  });
+  it("returns a generic error for unknown code", () => {
+    const body = {
+      message: "404 Page not found",
+      code: 88888,
+    };
+    const httpStatus = 404;
+    const response = {
+      ...defaultResponse,
+      ...{ httpStatus, body },
+    };
+    const error = parse(response);
+    assert.instanceOf(error, IdealPostcodesError);
   });
 
   describe("when 200", () => {
