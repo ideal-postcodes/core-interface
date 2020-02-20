14cc546f4ab8229b869b9d4b2b226fb30868ae6b
Christopher Blanchard <cablanchard@gmail.com>
2020-02-20 16:07:00 +0000

    Drop babel-polyfill for core-js and regen runtime

--- .config/karma.conf.ts
+++ .config/karma.conf.ts
@@ -1,4 +1,5 @@
-import "babel-polyfill";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import { Config } from "karma";
 import { executablePath } from "puppeteer";
 process.env.CHROME_BIN = executablePath();
