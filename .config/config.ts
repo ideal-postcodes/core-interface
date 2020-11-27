c007c46dcd443d1874fc6e793f4c2c41139f92e1
mfilip <marcin.filip@gmail.com>
2020-11-27 09:34:23 +0000

    update browsers support

--- .config/config.ts
+++ .config/config.ts
@@ -6,7 +6,7 @@
 import { config as dotenv } from "dotenv";
 dotenv();
 
-export const frameworks = ["mocha", "karma-typescript", "polyfill"];
+export const frameworks = ["mocha", "karma-typescript"];
 
 export const preprocessors = {
   "**/*.ts": ["karma-typescript"],
@@ -18,16 +18,19 @@ export const karmaTypescriptConfig = {
   },
   bundlerOptions: {
     exclude: ["@ideal-postcodes/api-typings"],
+    transforms: [require("karma-typescript-es6-transform")()],
   },
 };
 
-export const files = [{ pattern: "lib/**/*.ts" }, { pattern: "test/**/*.ts" }];
+export const files = [
+  "node_modules/@babel/polyfill/dist/polyfill.js",
+  { pattern: "lib/**/*.ts" },
+  { pattern: "test/**/*.ts" },
+];
 
 export const reporters = ["dots", "karma-typescript"];
 
 export const singleRun = true;
-
-export const polyfill = ["Promise"];
 
 export const basePath = "../";
 
