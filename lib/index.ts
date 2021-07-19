9717306545d6f61cd15ad7ea313727b3f3fd0446
Christopher Blanchard <git@nf3.co>
2021-07-19 16:10:29 +0200

    fix(Exports): Drop namespaced exports
    Does not work well on some setups

--- lib/index.ts
+++ lib/index.ts
@@ -6,12 +6,14 @@
 
 export { Client, Config, defaults } from "./client";
 export { Agent, HttpRequest, HttpResponse } from "./agent";
-export * as addresses from "./resources/addresses";
-export * as autocomplete from "./resources/autocomplete";
-export * as keys from "./resources/keys";
-export * as postcodes from "./resources/postcodes";
-export * as udprn from "./resources/udprn";
-export * as umprn from "./resources/umprn";
 export * from "./helpers";
 export * from "./types";
-export * as errors from "./error";
+import * as addresses from "./resources/addresses";
+import * as autocomplete from "./resources/autocomplete";
+import * as keys from "./resources/keys";
+import * as postcodes from "./resources/postcodes";
+import * as udprn from "./resources/udprn";
+import * as umprn from "./resources/umprn";
+import * as errors from "./error";
+
+export { addresses, autocomplete, keys, postcodes, udprn, umprn, errors };
